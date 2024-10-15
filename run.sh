6606e9ffdb7c26189a3455ab366220b9a2edfdd1
Stefan Lang <Stefan.Lang@med.lu.se>
2024-10-15 14:42:34 +0200

    Update run.sh
    Check if -B folders exist.

--- run.sh
+++ run.sh
@@ -12,7 +12,23 @@ if [ ! -f "${IMAGE_PATH}/${IMAGE_NAME}" ]; then
     exit 1
 fi
 
+# Define the folders to check and mount if exist
+# This only makes sense if they are mount points!
+FOLDERS=("/local" "/projects" "/mnt" )
+MOUNT_OPTIONS=""
+
+# Check if each folder exists and build the mount options
+for dir in "${FOLDERS[@]}"; do
+    if [ -d "$dir" ]; then
+        MOUNT_OPTIONS+="$dir,"
+    fi
+done
+
+if [ -n "$MOUNT_OPTIONS" ]; then
+        MOUNT_OPTIONS = "-B "+MOUNT_OPTIONS
+fi
+
 # Run the image
 echo "Running ${IMAGE_NAME}..."
-apptainer run -B /local,/projects --cleanenv "${IMAGE_PATH}/${IMAGE_NAME}"
+apptainer exec $MOUNT_OPTIONS --cleanenv "${IMAGE_PATH}/${IMAGE_NAME}" jupyter lab --port 9734 --ip=0.0.0.0 --allow-root --no-browser
 
